0c82aa35153c052409b2a2ea24bd1d68c90a971e
bidalu80-svg <bidalu80-svg@users.noreply.github.com>
2026-05-08 14:15:34 +0800

    Fix fakefsify CI helper linkage

--- scripts/prepare-local-alpine-fakefs.sh
+++ scripts/prepare-local-alpine-fakefs.sh
@@ -32,6 +32,11 @@ fi
   -I"$ish_dir" \
   "$ish_dir/tools/fakefsify.c" \
   "$ish_dir/tools/fakefs.c" \
+  "$ish_dir/kernel/log.c" \
+  "$ish_dir/kernel/task.c" \
+  "$ish_dir/kernel/user.c" \
+  "$ish_dir/util/fifo.c" \
+  "$ish_dir/util/sync.c" \
   "$ish_dir/util/fchdir.c" \
   "$ish_dir/fs/fake-db.c" \
   "$ish_dir/fs/fake-migrate.c" \
